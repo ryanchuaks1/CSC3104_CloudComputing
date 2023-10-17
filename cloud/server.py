c3ec1b4fac9ee1c72550b9bc69c822047f1ee28d
mcdulltii <mcdulltii@gmail.com>
2023-10-17 10:42:22 -0400

    Added gRPC for python server

--- cloud/server.py
+++ cloud/server.py
@@ -1,108 +1,88 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS, cross_origin
+from concurrent import futures
 from pymongo import MongoClient
 from bson import json_util
 from bson.objectid import ObjectId
 import json
 
-# Define the MongoDB connection string
-MONGO_URI = "mongodb://root:password@mongodb:27017/mongo_db?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=false"
-
-# Create the Flask app
-app = Flask(__name__)
-cors = CORS(app)
-app.config["CORS_HEADERS"] = "Content-Type"
-db = None
+import grpc
+import device_pb2, device_pb2_grpc
 
 
-def connect_db() -> MongoClient:
-    global MONGO_URI
-    try:
-        # Connect to MongoDB
-        client = MongoClient(MONGO_URI)
-        print("Connected to MongoDB successfully!")
-    except errors.ConnectionFailure as e:
-        print("Could not connect to MongoDB:", e)
-    except Exception as e:
-        print("An error occurred:", e)
-    
-    return client
+class Device(device_pb2_grpc.DeviceServicer):
+    def __init__(self) -> None:
+        # Define the MongoDB connection string
+        self.MONGO_URI = "mongodb://root:password@mongodb:27017/mongo_db?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=false"
 
 
-def close_db(client: MongoClient) -> None:
-    if client:
-        # Close the connection when done
-        client.close()
+    def connect_db(self) -> None:
+        # Connect to the MongoDB database
+        try:
+            # Connect to MongoDB
+            self.client = MongoClient(self.MONGO_URI)
+            print("Connected to MongoDB successfully!")
+        except Exception as e:
+            print("An error occurred:", e)
+
+        # Get the database
+        self.db = self.client.get_database()
 
 
-@app.route("/api/addNewDevice", methods=["POST"])
-@cross_origin()
-def add_new_device():
-    global db
-    device_data = request.get_json()
-    devices_collection = db.devices
-
-    # Add the new device to the collection
-    new_device = {"deviceId": device_data["deviceId"], "deviceName": device_data["deviceName"], "latitude": device_data["latitude"], "longitude": device_data["longitude"]}
-    devices_collection.insert_one(new_device)
-
-    return {"result": True, "message": "Device added successfully!"}, 201
+    def close_db(self) -> None:
+        if self.client:
+            # Close the connection when done
+            self.client.close()
 
 
-@app.route("/api/updateDevice", methods=["POST"])
-@cross_origin()
-def update_device():
-    global db
-    device_data = request.get_json()
-    devices_collection = db.devices
+    def add_new_device(self, request, context):
+        devices_collection = self.db.devices
 
-    # Update the existing device in the collection
-    update_query = {"_id": ObjectId(device_data["_id"])}
-    update_data = {"$set": {"deviceId": device_data["deviceId"]}}
-    devices_collection.update_one(update_query, update_data)
+        # Add the new device to the collection
+        new_device = {"deviceId": request.deviceId, "deviceName": request.deviceName, "latitude": request.latitude, "longitude": request.longitude}
+        devices_collection.insert_one(new_device)
 
-    return {"result": True, "message": "Device updated successfully!"}, 201
+        return device_pb2.Reply(result = "True", message = "Device added successfully!")
 
 
-@app.route("/api/deleteDevice", methods=["POST"])
-@cross_origin()
-def delete_device():
-    global db
-    device_data = request.get_json()
-    devices_collection = db.devices
+    def update_device(self, request, context):
+        device_data = request.get_json()
+        devices_collection = self.db.devices
 
-    # Delete the device from the collection
-    delete_query = {"_id": ObjectId(device_data["_id"])}
-    devices_collection.delete_one(delete_query)
+        # Update the existing device in the collection
+        update_query = {"_id": ObjectId(request._id)}
+        update_data = {"$set": {"deviceId": request.deviceId}}
+        devices_collection.update_one(update_query, update_data)
 
-    return {"result": True, "message": "Device deleted successfully!"}, 201
+        return device_pb2.Reply(result = "True", message = "Device updated successfully!")
 
 
-@app.route("/api/getAllDevices")
-@cross_origin()
-def get_all_devices():
-    global db
-    devices_collection = db.devices
+    def delete_device(self, request, context):
+        device_data = request.get_json()
+        devices_collection = self.db.devices
 
-    # Get all devices from the collection
-    all_devices = [json.loads(json_util.dumps(device)) for device in devices_collection.find()]
+        # Delete the device from the collection
+        delete_query = {"_id": ObjectId(request._id)}
+        devices_collection.delete_one(delete_query)
 
-    return all_devices, 200
+        return device_pb2.Reply(result = "True", message = "Device deleted successfully!")
+
+
+    def get_all_devices(self, request, context):
+        devices_collection = self.db.devices
+
+        # Get all devices from the collection
+        all_devices = [json.loads(json_util.dumps(device)) for device in devices_collection.find()]
+
+        return device_pb2.Reply(result = "True", message = "Devices retrieved successfully", items = all_devices)
 
 
 def main() -> None:
-    global db
-    # Connect to the MongoDB database
-    client = connect_db()
-    # Get the database
-    db = client.get_database()
-    while True:
-        try:
-            app.run(host="0.0.0.0", port=5000, debug=True)
-        except KeyboardInterrupt:
-            break
-    close_db(client)
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    device_pb2_grpc.add_DeviceServicer_to_server(Device(), server)
+    server.add_insecure_port('[::]:5000')
+    server.start()
+    server.wait_for_termination()
 
 
 if __name__ == '__main__':
     main()
+
