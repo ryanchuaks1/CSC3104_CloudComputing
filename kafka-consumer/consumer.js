4fef15240162165915fa1f6ab4b3ea62016d8e7f
leungwj <30183164+leungwj@users.noreply.github.com>
2023-10-27 17:11:41 +0800

    Restructured Kafka files, and added preliminary gRPC implementation on the Kafka Consumer

--- kafka-consumer/consumer.js
+++ kafka-consumer/consumer.js
@@ -1,35 +1,81 @@
-const uuid = "f1740855-6716-11ee-9b42-107b44"
+class Kafka_Consumer{
+    constructor(udid, client_id, group_id){
+        this._udid = udid;
+        const { Kafka } = require('kafkajs');
+        this._kafka_conn = new Kafka({
+            clientId: client_id,
+            brokers: ['kafka:9092']
+        })
+        this._consumer = this._kafka_conn.consumer({
+            groupId: group_id
+        })
+    }
 
-const { Kafka } = require('kafkajs');
-
-const kafka = new Kafka({
-    clientId: 'my-app',
-    brokers: ['kafka:9092'],
-});
-
-const consumer = kafka.consumer({
-    groupId: 'my-group'
-});
-
-const run = async () => {
-    await consumer.connect();
-    await consumer.subscribe({
-        topic: uuid,
-        fromBeginning: true
-    });
-
-    await consumer.run({
-        eachMessage: async ({ topic, partition, message }) => {
-            console.log({
-                key: message.key.toString(),
-                value: message.value.toString(),
+    async subscribe_and_listen(){
+        try{
+            await this._consumer.connect();
+            await this._consumer.subscribe({
+                topic: this._udid,
+                fromBeginning: false
+            });
+    
+            await this._consumer.run({
+                eachMessage: async({ topic, partition, message }) => {
+                    this.return_location(topic, partition, message);
+                }
             })
-        },
-    });
+        }
+        catch(error){
+            console.error("Error: " + error.message);
+        }
+        finally{
+            await this._consumer.disconnect();
+        }
 
-    console.log("hello");
+    }
+
+    return_location(topic, partition, message){
+        console.log({
+            key: message.key.toString(),
+            value: message.value.toString(),
+        })
+    }
 }
 
-console.log("Hello, Consumer!");
+module.exports = { Kafka_Consumer };
 
-run().catch(console.error);
+//function subscribe(udid, )
+
+//const udid = "f1740855-6716-11ee-9b42-107b44"
+
+// const { Kafka } = require('kafkajs');
+
+// const kafka = new Kafka({
+//     clientId: 'my-app',
+//     brokers: ['kafka:9092'],
+// });
+
+// const consumer = kafka.consumer({
+//     groupId: 'my-group'
+// });
+
+// const run = async () => {
+//     await consumer.connect();
+//     await consumer.subscribe({
+//         topic: udid,
+//         fromBeginning: true
+//     });
+
+//     await consumer.run({
+//         eachMessage: async ({ topic, partition, message }) => {
+//             console.log({
+//                 key: message.key.toString(),
+//                 value: message.value.toString(),
+//             })
+//         },
+//     });
+// }
+
+// console.log("Hello, Consumer!");
+
+// run().catch(console.error);
