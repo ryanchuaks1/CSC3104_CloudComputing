daf08bff02000958374a0e4dee1d8ac9038b349c
woshityj <yujietan84@gmail.com>
2023-10-27 19:36:31 +0800

    Sending HWID of Pico and AES CBC Encryption

--- bluetooth_embedded/main.py
+++ bluetooth_embedded/main.py
@@ -2,18 +2,63 @@ import bluetooth
 from ble_simple_peripheral import BLESimplePeripheral
 import time
 import machine
+import ubinascii
+import uos
+from ucryptolib import aes
 
-ble = bluetooth.BLE()
+MODE_ECB = 1
+MODE_CBC = 2
+MODE_CTR = 6
+BLOCK_SIZE = 16
+key = b'E8B6C00C9ADC5E75BB656ECD429CB164'
+iv = b'3A25B111FCD22C66'
 
-sp = BLESimplePeripheral(ble)
-
-debounce_time = 0
-
-while True:
+def encrypt(key, iv, message):
+    pad = BLOCK_SIZE - len(message) % BLOCK_SIZE
+    plaintext = message + " "*pad
     
-    # Check if the BLE connection is established
+    cipher = aes(key, MODE_CBC, iv)
+    ct_bytes = iv + cipher.encrypt(plaintext)
+    
+    return ct_bytes
+
+def decrypt(key, iv, cipher_bytes):
+    iv = cipher_bytes[:BLOCK_SIZE]
+    cipher = aes(key, MODE_CBC, iv)
+    decrypted = cipher.decrypt(ct_bytes)[BLOCK_SIZE:]
+    
+    return decrypted
+
+    
+
+#plaintext = 'This is AES cryptographic'
+#ct_bytes = encrypt(key, iv, plaintext)
+#decrypted = decrypt(key, iv, ct_bytes)
+#print('AES-CBC decrypted:', decrypted)
+
+def bluetooth_init():
+    ble = bluetooth.BLE()
+    
+    sp = BLESimplePeripheral(ble)
+    
+    return sp
+
+def advertise_bluetooth_message(sp, message):
+    
+    # Checks if a device is connected
+    #
     if sp.is_connected():
-        msg = machine.unique_id()
-        sp.send(msg)
+        print(message)
+        sp.send(message)
+    
+    return;
+
+sp = bluetooth_init()
+hwid = machine.unique_id()
+ct_bytes = encrypt(key, iv, hwid)
+ct = ubinascii.hexlify(ct_bytes).decode('utf-8')
+print(ct)
+while True:
+    advertise_bluetooth_message(sp, ct)
     
     time.sleep(5)
