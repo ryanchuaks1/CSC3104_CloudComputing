80fd2a7152912449ead220a17730a0eb10764aad
ChuaJoshua <2837319C@student.gla.ac.uk>
2023-10-28 13:17:00 +0800

    Added Flutter Consumer Comments

--- flutter_app/lib/src/core/services/consumer/kafka_consumer_handler.dart
+++ flutter_app/lib/src/core/services/consumer/kafka_consumer_handler.dart
@@ -13,17 +13,20 @@ import 'package:flutter_app/src/core/services/consumer/kafka_consumer.pbgrpc.dar
 
 import 'package:grpc/grpc.dart';
 
+//For Testing/Debugging Purposes
 void main() async {
   
-  print("Print Hello...\n");
+  print("Start Program...\n");
+
   try {
     KafkaConsumerHandler handler = KafkaConsumerHandler();
     if (handler.startConsumerService('127.0.0.1', 50051)) {
-      await handler.initSubscription("f1740855-6716-11ee-9b42-107b44");
+      await handler.subscribeToDevice("f1740855-6716-11ee-9b42-107b44");
       handler.closeConnection();
       
     }
   } catch (error) {
+    
     print("Error Executing");
   }
 
@@ -31,17 +34,22 @@ void main() async {
 
 class KafkaConsumerHandler
 {
+  //Class Variables
   late Kafka_Consumer_gRPCClient _stub;
   late ClientChannel channel;
 
+  //Initialising gRPC connection with the consumer server
   bool startConsumerService(String KafkaServerIp, int portNumber) 
   {
     try {
+
+      //Create the Channel
       channel = ClientChannel(KafkaServerIp,
         port: portNumber,
         options:
             const ChannelOptions(credentials: ChannelCredentials.insecure()));
 
+      //Create the stub
       _stub = Kafka_Consumer_gRPCClient(channel);
       return true;
 
@@ -52,12 +60,16 @@ class KafkaConsumerHandler
  
   }
 
-  Future<void> initSubscription(String deviceId) async
+  // This functions allows you to subscribe to a device and wait for a reply
+  Future<void> subscribeToDevice(String deviceId) async
   {
+    // Add the paremeters to the message
     final params = Subscribe_Data()..udid=deviceId;
 
+    //Call subscribe and wait for a reply
     final stream = await _stub.subscribe(params);
 
+    //Print the Reply
     print('Greeter client received: ${stream.udid}, ${stream.timestamp}, ${stream.location}');
 
     // await for (var message1 in stream) {
@@ -67,6 +79,7 @@ class KafkaConsumerHandler
     // }
   }
 
+  //This function is to close the channel
   Future<void> closeConnection() async
   {
     await channel.shutdown();
