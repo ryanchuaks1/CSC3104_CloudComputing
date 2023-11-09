33100147cfc9c3e01298d02b41cd54d899fc4f88
ChuaJoshua <2837319C@student.gla.ac.uk>
2023-11-09 18:52:23 +0800

    Deleted unneccessary files

--- flutter_app/lib/src/core/models/user_model.dart
+++
@@ -1,9 +0,0 @@
-class User_Account {
-  late String userName;
-  late String userPasswordHash;
-
-  User_Account({
-    required this.userName,
-    required this.userPasswordHash,
-  });
-}
--- flutter_app/lib/src/ui/views/Welcome/welcome_page.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_app/src/ui/views/Welcome/components/body.dart';
-
-class WelcomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(body: Body());
-  }
-}
